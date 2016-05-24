414c5f53788205f3bebb892da86c688a05f744e8
Cheryl <Cheryl>
2016-05-24 13:49:32 -0400

    Added config.js and app.js to the js folder

---
+++ public/assignment/js/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created on 5/24/2016.
+ */
+
+(function(){
+    angular.module("WebAppMaker", ["ngRoute"]);
+})();
---
+++ public/assignment/js/config.js
@@ -0,0 +1,19 @@
+/**
+ * Created on 5/24/2016.
+ */
+
+(function(){
+    angular
+        .module("WebAppMaker")
+        .config(Config);
+
+    function Config($routeProvider) {
+        $routeProvider
+            .when("/login", {
+                templateUrl: "views/user/login.view.client.html"
+            })
+            .when("/register", {
+                templateUrl: "views/user/register.view.client.html"
+            });
+    }
+})();
