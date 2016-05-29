f690688735c7c98c58272c7e60ceacc202bc5338
Cheryl <Cheryl>
2016-05-29 16:19:51 -0400

    Added the register controller to the config file, created the register controller and amended the register view to use it

--- public/assignment/views/user/register.controller.client.js
+++ public/assignment/views/user/register.controller.client.js
@@ -1,3 +1,23 @@
-/**
- * Created by a on 5/29/2016.
- */
+(function(){
+    angular
+        .module("WebAppMaker")
+        .controller("RegisterController", RegisterController);
+
+    function RegisterController($location, UserService) {
+
+        var vm = this;
+
+        vm.register = register;
+
+        function register (username, password, verifyPassword) {
+            if (username === UserService.findUserByUsername(username)) {
+                vm.error = "Username already taken";
+            } else if (password != verifyPassword) {
+                vm.error = "Passwords did not match";
+            } else {
+                UserService.createUser(username);
+                $location.url("/user/" + id);
+            }
+        }
+    }
+})();
