9674dccd5bf591ee5990f7d204fab4439d7629aa
Cheryl <Cheryl>
2016-05-30 23:26:34 -0400

    Created page-new controller, update function not correct

--- public/assignment/views/page/page-new.controller.client.js
+++ public/assignment/views/page/page-new.controller.client.js
@@ -1,3 +1,17 @@
-/**
- * Created by a on 5/29/2016.
- */
+(function(){
+    angular
+        .module("WebAppMaker")
+        .controller("PageNewController", PageNewController);
+
+    function PageNewController($location, PageService) {
+
+        var vm = this;
+
+        vm.updatePage = updatePage;
+
+        function updatePage (pageId, page) {
+            var page = PageService.updatePage(pageId, page);
+            $location.url("/user/:uid/website/:wid/page/:pid");
+        }
+    }
+})();
