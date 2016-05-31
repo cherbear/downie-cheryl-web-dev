76e970d13dd170a0ab1a5c4e2079b765d88ecb1f
Cheryl <Cheryl>
2016-05-30 23:18:59 -0400

    Created page-edit controller and modified page-edit view to make the delete function

--- public/assignment/views/page/page-edit.controller.client.js
+++ public/assignment/views/page/page-edit.controller.client.js
@@ -1,3 +1,17 @@
-/**
- * Created by a on 5/29/2016.
- */
+(function(){
+    angular
+        .module("WebAppMaker")
+        .controller("PageEditController", PageEditController);
+
+    function PageEditController($location, PageService) {
+
+        var vm = this;
+
+        vm.deletePage = deletePage;
+
+        function deletePage (pageId) {
+            var page = PageService.deletePage(pageId);
+            $location.url("/user/:uid/website/:wid/page");
+        }
+    }
+})();
