fe9d6e2f57b8e6b0eb568c9b7cbd4370ef0acb4f
Cheryl <Cheryl>
2016-05-31 13:58:18 -0400

    Added in the server.js to look for assignment

--- server.js
+++ server.js
@@ -13,4 +13,9 @@ require ("./test/app.js")(app);
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-app.listen(port, ipaddress);
+// app.listen(port, ipaddress); this stuff might not be necessary
+
+
+var assignment = require('./assignment/app.js');
+
+app.listen(3000);
