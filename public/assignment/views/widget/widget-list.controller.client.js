52f22b8f4b1471a82926ea92f67eae72dde843d1
Cheryl <Cheryl>
2016-05-31 00:44:35 -0400

    Added widget-list controller

--- public/assignment/views/widget/widget-list.controller.client.js
+++ public/assignment/views/widget/widget-list.controller.client.js
@@ -1,3 +1,20 @@
-/**
- * Created by a on 5/29/2016.
- */
+(function(){
+    angular
+        .module("WebAppMaker")
+        .controller("WidgetListController", WidgetListController);
+
+    function WidgetListController($routeParams, WebsiteService) {
+        var vm = this;
+        vm.widgetId = $routeParams.widgetId;
+        vm.widgetType = $routeParams.widgetType;
+
+        function init() {
+            vm.widget = WidgetService.findWidgetById(vm.widgetId);
+        }
+        init();
+
+        function create(widgetType) {
+            vm.widget = WidgetService.createWidget(vm.pageId, vm.widget);
+        }
+    }
+})();
