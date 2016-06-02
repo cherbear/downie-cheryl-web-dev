1bfa33d5ccdee675438d764a0ec31cc8ad37276a
Cheryl <Cheryl>
2016-06-02 14:31:08 -0400

    Added the server services and altered app.js to match the assignment document

--- assignment/app.js
+++ assignment/app.js
@@ -1,6 +1,18 @@
 module.exports = function(app) {
+    require("services/user.service.server.js")(app);
+    require("services/website.service.server.js")(app);
+    require("services/page.service.server.js")(app);
+    require("services/widget.service.server.js")(app);
+};
+
+/* Below is what we monkeyed with in class
+
+
+module.exports = function(app) {
     app.get("/say/:message", function(req, res) {  //req = request, res = response
         var msg = req.params["message"];
         res.send("This is a message: " + msg);
     });
 };
+
+    */
