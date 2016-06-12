0b25a34b287e7429ad7f98160cd40c6dde716d25
Cheryl <Cheryl>
2016-06-12 16:05:22 -0400

    Adjusted app.js to use the models that connect to mongo and added said models

--- assignment/app.js
+++ assignment/app.js
@@ -1,8 +1,31 @@
 module.exports = function(app) {
 
-    var userService = require("./services/user.service.server.js")(app);
-    var widgetService = require("./services/widget.service.server.js")(app);
-    //var pageService = require("./services/page.service.server.js")(app);
-    //var websiteService = require("./services/widget.service.server.js")(app);
+    var models = require("./models/models.js")();
 
+    var userService = require("./services/user.services.server.js")(app, models);
+    var websiteService = require("./services/website.service.server")(app, models);
+    var widgetService = require("./services/widget.service.server.js")(app, models);
+    var todoService = require("./services/todo.service.server")(app);
+
+    var users = [
+        {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
+        {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
+        {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
+        {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
+    ];
+
+    app.get("/allusers/:username", function(req, res){
+        var username = req.params['username'];
+        for(var i in users) {
+            if(users[i].username === username) {
+                res.send(users[i]);
+            }
+        }
+//        res.send(users);
+    });
+
+    app.get("/say/:message", function(req, res) {
+        var msg = req.params["message"];
+        res.send({message: msg});
+    });
 };
