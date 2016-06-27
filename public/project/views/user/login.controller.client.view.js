160926eee0b9f7f02e9113230f55c4704a60051b
Cheryl <Cheryl>
2016-06-27 16:16:18 -0400

    Fixed controllers to be js

---
+++ public/project/views/user/login.controller.client.view.js
@@ -0,0 +1,31 @@
+(function(){
+    angular
+        .module("ServiceMeetUps")
+        .controller("LoginController", LoginController);
+
+    function LoginController($location, $rootScope, UserService) {
+
+        var vm = this;
+
+        vm.login = login;
+
+        function login (username, password) {
+            UserService
+                .login(username, password)
+                .then(
+                    function(response) {
+                        console.log(response);
+                        var user = response.data;
+                        if(user) {
+                            $rootScope.currentUser = user;
+                            var id = user._id;
+                            $location.url("/user/" + id);
+                        }
+                    },
+                    function (error) {
+                        vm.error = "User not found";
+                    }
+                );
+        }
+    }
+})();
