f5e5f836163ea48e8b5b17c5b87d46fb1332cfe6
Cheryl <Cheryl>
2016-06-27 00:06:19 -0400

    Homepage

--- public/project/config.js
+++ public/project/config.js
@@ -1,3 +1,52 @@
-/**
- * Created by a on 6/23/2016.
- */
+(function(){
+    angular
+        .module("WebAppMaker")
+        .config(Config);
+
+    function Config($routeProvider) {
+        $routeProvider
+            .when("/login", {
+                templateUrl: "views/user/login.view.client.html",
+                controller: "LoginController",
+                controllerAs: "model"
+            })
+            .when("/", {
+                templateUrl: "views/user/login.view.client.html",
+                controller: "LoginController",
+                controllerAs: "model"
+            })
+            .otherwise({
+                redirectTo: "/login"
+            });
+
+        function checkLoggedin(UserService, $q, $location, $rootScope) {
+
+            var deferred = $q.defer();
+
+            UserService
+                .checkLoggedin()
+                .then(
+                    function(response) {
+                        var user = response.data;
+                        console.log(user);
+                        if(user == '0') {
+                            deferred.reject();
+                            $rootScope.currentUser = null;
+                            $location.url("/login")
+                        } else {
+                            $rootScope.currentUser = user;
+                            deferred.resolve();
+                        }
+                    },
+                    function(err) {
+                        console.log(err);
+                        $rootScope.currentUser = null;
+                        deferred.reject();
+                    }
+                );
+
+            return deferred.promise;
+        }
+
+    }
+})();
