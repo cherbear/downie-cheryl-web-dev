52f7192cc3c163b5d1179a91cc260404704dc0e4
Cheryl <Cheryl>
2016-06-29 18:48:18 -0400

    connecting to server

--- project/app.js
+++ project/app.js
@@ -1,4 +1,7 @@
 module.exports = function(app) {
 
     var models = require("./models/models.js")();
+
+    var userService = require("./services/user.service.server.js")(app, models);
+    var eventService = require("./services/event.service.server.js")(app, models);
 };
