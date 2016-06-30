a41f8967b9eba7fbee9cb0fd51b4478c53a44f6a
Cheryl <Cheryl>
2016-06-30 11:28:21 -0400

    event schema

--- project/models/event/event.schema.server.js
+++ project/models/event/event.schema.server.js
@@ -1,3 +1,17 @@
-/**
- * Created by a on 6/29/2016.
- */
+module.exports = function() {
+    var mongoose = require("mongoose");
+
+    var EventSchema = mongoose.Schema({
+        name: String,
+        description: String,
+        dateCreated: {type: Date, default: Date.now},
+        dateOfEvent: {type: Date},
+        usersAttending: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+        address: String,
+        contactEmail: String,
+        contactPhone: String,
+        needHelp: Boolean
+    }, {collection: "project.user"});
+
+    return EventSchema
+};
